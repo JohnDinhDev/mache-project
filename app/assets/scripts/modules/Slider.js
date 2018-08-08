7e91cd26246bb5b56d860be9bddaa80cb89d63a0
John <johnduydinh@gmail.com>
2018-08-08 04:18:52 -0400

    Completed styling project

--- app/assets/scripts/modules/Slider.js
+++ app/assets/scripts/modules/Slider.js
@@ -2,12 +2,19 @@ import { tns } from '../../../../node_modules/tiny-slider/src/tiny-slider'
 
 class Slider {
     constructor() {
-        this.container = document.querySelector('.slider')
+        this.screenSlider = document.querySelector('.slider--screen')
         this.slider()
     }
 
     slider() {
         const slider = tns({
+            speed: 800,
+            nav: false,
+            autoplay: true,
+        })
+
+        const slider2 = tns({
+            container: this.screenSlider,
             speed: 800,
             nav: false,
             autoplay: true,
